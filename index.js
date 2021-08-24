dde3b376deb40f8aa63ac4113ee650944800884c
olivier bachard <olivier.bachard@lisymarket.com>
2021-08-24 10:48:26 +0200

    project init

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * Setup
+ * Open a windows terminal and browse to the current directory.
+ * Run the command "node index.js" and assert that the message "Hello" display correctly.
+ */
+console.log("Hello !");
+
+
+/**
+ * Exercice 1
+ * Write a program that prints the numbers from 1 to 100. 
+ * But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+ * For numbers which are multiples of both three and five print “FizzBuzz”.
+ */
+
+
+
+/**
+ * Excercice 2
+ * A palindrome is a word which reads the same backward or forward, ignoring punctuation, case, and spacing. 'abcba' is a palindrome.
+ * Palindrome
+ * Write a method that detects if a string is a palindrome. 
+ * For each word in the array "testCases", the method should return true if the word is a palindrome, false otherwise.
+ */
+var testCases = ["racecar", "nope", "z", "almostomla", "RaceCar", "not a palindrome", "A man, a plan, a canal. Panama", "never odd or even",  "My age is 0 0 si ega ym", "1 eye for of 1 eye"];
+
+
