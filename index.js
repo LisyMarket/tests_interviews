6f4ab99f10a72dd63d479fdccc4a00e879eab7c3
olivier bachard <olivier.bachard@lisymarket.com>
2021-09-20 16:46:35 +0200

    update test

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ console.log("Hello !");
 /**
  * Exercice 1
  * Write a program that prints the numbers from 1 to 100. 
- * But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
- * For numbers which are multiples of both three and five print “FizzBuzz”.
+ * But for multiples of three print "Foo" instead of the number and for the multiples of five print "Bar". 
+ * For numbers which are multiples of both three and five print "FooBar".
  */
 
 
